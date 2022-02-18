9029ea9ae7711c982fb706e8d5d5c0807b1573a8
Sahachok.J <sahachok.j@kbtg.tech>
2022-02-18 14:32:33 +0700

    Generate pojo from json

---
+++ api.md
@@ -0,0 +1,11 @@
+##API specification
+
+## endpoint ?
+GET /demo/{id}
+
+## request ?
+## response ?
+{
+"code": 200,
+"message": "Demo with id={id}"
+}
---
+++ src/main/java/com/example/day02/DemoController.java
@@ -0,0 +1,14 @@
+package com.example.day02;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/demo/{id}")
+    public DemoResponse xxxx(@PathVariable int id){
+        return null;
+    }
+}
