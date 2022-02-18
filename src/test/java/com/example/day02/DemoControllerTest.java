fb9d196705ab6cf953f4d542de8ad04b6b997708
Sahachok.J <sahachok.j@kbtg.tech>
2022-02-18 16:32:07 +0700

    Fix expected test result

--- src/test/java/com/example/day02/DemoControllerTest.java
+++ src/test/java/com/example/day02/DemoControllerTest.java
@@ -19,7 +19,7 @@ class DemoControllerTest {
         //Act :: call api
         DemoResponse result = restTemplate.getForObject("/demo/1", DemoResponse.class);
         //Assert :: Verify
-        assertEquals(0,result.getCode());
-        assertNull(result.getMessage());
+        assertEquals(1,result.getCode());
+        assertEquals("Result with id = 1",result.getMessage());
     }
 }
