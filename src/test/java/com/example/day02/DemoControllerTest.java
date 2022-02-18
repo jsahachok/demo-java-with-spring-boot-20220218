c4b9f69de35ade622f9f6d2e92d158dc5bccb3c6
Sahachok.J <sahachok.j@kbtg.tech>
2022-02-18 16:43:26 +0700

    Working with Mockito

--- src/test/java/com/example/day02/DemoControllerTest.java
+++ src/test/java/com/example/day02/DemoControllerTest.java
@@ -1,11 +1,13 @@
 package com.example.day02;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.boot.test.context.SpringBootTest.*;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -13,6 +15,10 @@ class DemoControllerTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
+
+    @MockBean
+    private DemoService demoService;
+
 
     @Test
     void case01(){
